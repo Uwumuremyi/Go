6b8227b940760f38f4da43a624876728155cc479
Jean Uwumuremyi <jeanu28@hotmail.com>
2020-02-22 21:42:00 -0500

    You're doing great

---
+++ fmtpackage.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var y int = 42
+
+func main() {
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	fmt.Printf("%#x\n", y)
+}
